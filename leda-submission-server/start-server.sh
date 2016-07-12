dc389b261d1c425f908d375c75689e372e6bb3ea
adalberto <adalberto@computacao.ufcg.edu.br>
2016-07-12 09:39:22 -0300

    Modificado start.sh para rodar em background.

--- leda-submission-server/start-server.sh
+++ leda-submission-server/start-server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /home/ubuntu/leda/leda-tools/leda-submission-server
-mvn jooby:run -l /home/ubuntu/leda/leda-tools/leda-submission-server/log/server-output-$(date "+%Y.%m.%d-%H.%M.%S").log
+mvn jooby:run -l /home/ubuntu/leda/leda-tools/leda-submission-server/log/server-output-$(date "+%Y.%m.%d-%H.%M.%S").log &
