34252906be7d6be8a2302021cde5725395e59f04
adalberto <adalberto@computacao.ufcg.edu.br>
2016-07-12 09:23:01 -0300

    atualizado o start-server.sh

--- leda-submission-server/start-server.sh
+++ leda-submission-server/start-server.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mvn jooby:run -l server-output-$(date "+%Y.%m.%d-%H.%M.%S").log
+mvn jooby:run -l /home/ubuntu/leda/leda-tools/leda-submission-server/log/server-output-$(date "+%Y.%m.%d-%H.%M.%S").log
