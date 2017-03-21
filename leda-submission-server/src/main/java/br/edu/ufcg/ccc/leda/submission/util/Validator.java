5acca7badbc75ebc2a93fd58a1ece0b7c8915830
adalberto <adalberto@computacao.ufcg.edu.br>
2017-03-21 10:35:00 -0300

    Mudanças no validator par anao receber envios de reposicoes para as quais alunos fizeram o download das provas práticas.

--- leda-submission-server/src/main/java/br/edu/ufcg/ccc/leda/submission/util/Validator.java
+++ leda-submission-server/src/main/java/br/edu/ufcg/ccc/leda/submission/util/Validator.java
@@ -189,8 +189,12 @@ public class Validator {
 			//ele fazer download 
 			//se id for de uma prova de revisao tem que ver na prova correspondente a ela
 			if(Constants.PATTERN_PROVA_REPOSICAO.matcher(id).matches()){
-				String idProvaPratica = "PP" + id.charAt(2);
+				String idProvaPratica = "PP" + id.substring(2);
 				List<String> fizeramDownload = Util.alunosDownload(idProvaPratica);
+				if(fizeramDownload.size() == 0){
+					throw new AtividadeException("Registro de downloads da prova " 
+							+ idProvaPratica + " não encontrados. Favor contactar o prof. com urgencia!");										
+				}
 				if(fizeramDownload.contains(config.getMatricula())){
 					throw new AtividadeException("Ja existe um registro de download da prova " 
 						+ idProvaPratica + " para o estudante " + config.getMatricula() + ". Envio da prova de reposicao nao permitido!!! ");					
