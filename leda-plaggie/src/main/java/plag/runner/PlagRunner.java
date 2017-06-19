c7f90d16397ce2d0edf8a0c491120cb14ccb4d6e
adalberto <adalberto@computacao.ufcg.edu.br>
2017-06-19 17:29:57 -0300

    Insercao de metodos para integrar o plaggie com as ferramentas.

--- leda-plaggie/src/main/java/plag/runner/PlagRunner.java
+++ leda-plaggie/src/main/java/plag/runner/PlagRunner.java
@@ -1,13 +1,121 @@
 package plag.runner;
 
+import java.io.File;
+import java.io.FileFilter;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
+import br.edu.ufcg.ccc.leda.util.Student;
 import plag.parser.CachingDetectionResult;
 import plag.parser.SubmissionDetectionResult;
 import plag.parser.plaggie.Plaggie;
 
 public class PlagRunner {
+	
+	/**
+	 * Recebe a pasta de submissoes dos alunos e compara os arquivos fornecidos em 
+	 * determinada lista de arquivos dada por fileNames.
+	 * 
+	 * @param submissionsFolder
+	 * @param fileNames
+	 */
+	public static void plagAnalysis(File submissionsFolder,String[] fileNames, Map<String, Student> students){
+		//precisa: usar uma pasta temporaria (cria uma pasta analysis na pasta da atividade)
+		//criar uma pasta para cada aluno (pode ser a matricula)
+		//copiar apenas os arquivos especificos informados na lista
+		// para a pasta do aluno correspondente. 
+		
+		//depois roda a analise na pasta de analises gerada. cria uma configuracao
+		//padrao ou especifica 
+	}
+	
+
+	/**
+	 * Cria a pasta de analise de um estudante e ja copia os arquivos pra la. 
+	 *  //nome da pasta de analises pode ser uma constante global
+		//cria uma subpasta "analysis" na pasta pai de submissionsFolder
+		//varre as subpastas de submissionsFolder (com as submissoes dos alunos)
+		//copia de cada subpasta os arquivos informados em fileNames (busca recursiva)
+
+	 * @param studentFolder
+	 * @return
+	 */
+	private static File createAnalysisFolder(File submissionsFolder, String[] fileNames){
+		File analysisFolder = new File(submissionsFolder.getParentFile(),"analysis");
+		if(!analysisFolder.exists()){
+			analysisFolder.mkdirs();
+		}
+		File[] subFolders = submissionsFolder.listFiles(new FileFilter() {
+			
+			@Override
+			public boolean accept(File pathname) {
+				return pathname.isDirectory();
+			}
+		});
+		if(subFolders.length > 0){
+			for (int i = 0; i < subFolders.length; i++) {
+				File studentFolder = subFolders[i];
+				File studentAnalysisSubFolder = 
+						new File(analysisFolder,studentFolder.getName());
+				if(!studentAnalysisSubFolder.exists()){
+					studentAnalysisSubFolder.mkdirs();
+				}
+				for (int j = 0; j < fileNames.length; j++) {
+					String fileName = fileNames[j];
+					File found = searchFile(studentFolder, fileName);
+					if(found != null){
+						//copia normalmente para a pasta do aluno 
+						//usar Files.copy
+					}
+				}
+			}
+		}
+		return analysisFolder;
+	}
+	/**
+	 * Busca um arquivo em uma pasta recursivamente e retorna o arquivo ou null.
+	 * 
+	 * @param folder
+	 * @param fileName
+	 * @return
+	 */
+	private static File searchFile(File folder, String fileName){
+		File found = null;
+		if(folder.isDirectory()){
+			//pega todos os arquivos normais e tenta ver se fileName esta la
+			File[] realFiles = folder.listFiles(new FileFilter() {
+				
+				@Override
+				public boolean accept(File pathname) {
+					return pathname.isFile() && pathname.getName().endsWith(fileName);
+				}
+			});
+			
+			if(realFiles.length == 1){
+				found = realFiles[0];
+			}else{ //nao esta na pasta atual e deve buscar nas subpastas
+				//pega todos as subpastas e tenta ver se fileName esta la recursivamente
+				File[] subFolders = folder.listFiles(new FileFilter() {
+					
+					@Override
+					public boolean accept(File pathname) {
+						return pathname.isDirectory();
+					}
+				});
+				if(subFolders.length > 0){
+					for (int i = 0; i < subFolders.length; i++) {
+						found = searchFile(subFolders[i], fileName);
+						if(found != null){
+							break;
+						}
+					}
+				}
+			}
+		}
+		return found;
+	}
+	
 	public static void main(String[] args) {
 		Plaggie plag = new Plaggie();
 		String folder = "D:\\UFCG\\leda\\leda-tools\\leda-plaggie\\src\\main\\resources\\subs";
