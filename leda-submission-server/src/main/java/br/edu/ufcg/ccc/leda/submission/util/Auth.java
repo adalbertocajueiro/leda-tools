6d119af53cbb238f7972c520bd8eb312c2950cf3
Héricles Emanuel <hericles.me@gmail.com>
2018-11-27 15:09:48 -0300

    validate auth

--- leda-submission-server/src/main/java/br/edu/ufcg/ccc/leda/submission/util/Auth.java
+++ leda-submission-server/src/main/java/br/edu/ufcg/ccc/leda/submission/util/Auth.java
@@ -1,49 +1,50 @@
-/*package br.edu.ufcg.ccc.leda.submission.util;
+package br.edu.ufcg.ccc.leda.submission.util;
 
-import java.util.Collections;
+import java.util.List;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-import com.fasterxml.jackson.core.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
 
 public class Auth {
 
-	private final String CLIENT_ID = "93476930298-p85cf2038thmgh4dljbmb1ujmsppp5t5.apps.googleusercontent.com";
+    private final Logger LOGGER = Logger.getLogger(Auth.class.getName());
 
-	HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-	JacksonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+    private final static String CLIENT_ID1 = "93476930298-dcdjdcu4uk9kb9892isecsl80go05mlo.apps.googleusercontent.com";
+    private final static String CLIENT_ID2 = "93476930298-j8k9746bc7kiqk82npbs0hkiillvat1a.apps.googleusercontent.com";
+    private final static String CLIENT_ID3 = "93476930298-grmvro192611ftfnhbgs7ll3ivpdqgip.apps.googleusercontent.com";
 
-	GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(httpTransport, jsonFactory)
-			.setAudience(Collections.singletonList(CLIENT_ID)).build();
+    private Payload getPayload(List<String> ClientIds, String idTokenString) {
 
-	// (Receive idTokenString by HTTPS POST)
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), JacksonFactory.getDefaultInstance())
+                .setAudience(ClientIds).build();
 
-	GoogleIdToken idToken = verifier.verify("idTokenString") 
+        GoogleIdToken idToken;
 
-	if(idToken != null) {
-		Payload payload = idToken.getPayload();
+        try {
+            System.out.println(idTokenString);
+            idToken = verifier.verify(idTokenString);
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
+            return null;
+        }
+        if (idToken != null) {
+            return idToken.getPayload();
+        } else {
+            LOGGER.log(Level.WARNING, "Invalid ID token.");
+            return null;
+        }
+    }
 
-		// Print user identifier
-		String userId = payload.getSubject();
-		System.out.println("User ID: " + userId);
-
-		// Get profile information from payload
-		String email = payload.getEmail();
-		boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
-		String name = (String) payload.get("name");
-		String pictureUrl = (String) payload.get("picture");
-		String locale = (String) payload.get("locale");
-		String familyName = (String) payload.get("family_name");
-		String givenName = (String) payload.get("given_name");
-
-		// Use or store profile information
-		// ...
-
-	} else {
-		System.out.println("Invalid ID token.");
-	}
-}*/
+/*    public static void main(String args[]) {
+        Auth a = new Auth();
+        String idTokenString = "Id Token received by POST";
+        Payload p = a.getPayload(Arrays.asList(CLIENT_ID1, CLIENT_ID2, CLIENT_ID3), idTokenString);
+        System.out.println(Boolean.valueOf(p.getEmailVerified()));
+    } For Testing */
+}
